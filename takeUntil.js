05569785490914c51c6382d47eeee13101e0313d
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-02 17:15:49 -0600

    Take Until and Lint

--- takeUntil.js
+++ takeUntil.js
@@ -9,6 +9,21 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
+const eqArrays = function(arrayOne, arrayTwo) {
+  let i = 0;
+  while (i < arrayOne.length || i < arrayTwo.length) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arrayOne, arrayTwo) => eqArrays(arrayOne, arrayTwo) ? console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`) : console.log(`🔴🔴🔴 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+
+
+
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
@@ -18,3 +33,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
