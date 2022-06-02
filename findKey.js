de8139f0be31d3fb21703ac430bb6de5f495e79a
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-02 17:26:17 -0600

    Key finder

---
+++ findKey.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  let keysList = Object.keys(object);
+  for (const key of keysList) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+let results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(results, "noma");
