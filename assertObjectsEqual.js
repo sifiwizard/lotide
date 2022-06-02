8be140e0df25507da553556454f05f7155c18c5a
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-01 18:03:39 -0600

    Linting and Object eq

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -34,6 +34,15 @@ const eqObjects = function(object1, object2) {
   return false;
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
@@ -47,3 +56,5 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+assertObjectsEqual(ab, ba);
