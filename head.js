591e80529a230b3cf25dd44f3903d0babd484e0e
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-06 16:02:00 -0600

    Testing ad refactoing

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const head = first => first[0];
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
